c4dedb9beae529980aad8086aa52371d807a49be
Riya Manchanda <59563727+pi34@users.noreply.github.com>
2022-01-28 14:41:00 +0530

    Update periodicStrings.cpp

--- periodicStrings.cpp
+++ periodicStrings.cpp
@@ -8,7 +8,7 @@ typedef long long ll;
 int modExp (int pow, int m) 
 {
     int res = 1;
-    for (int i = 1; i <= pow; ++i) {
+    for (int i = 1; i <= pow; i++) {
         res = res * 2;
         res = res % m;
     }
@@ -20,15 +20,9 @@ int main ()
     ll n, m;
     cin >> n >> m;
 
-    ll powers[n];
-
-    for (int i = 0; i <= n; ++i) {
-        powers[i] = modExp(i, m);
-    }
-
     vector<ll> factors;
 
-    for (ll i = 1; i <= n; ++i) {
+    for (ll i = 1; i <= n; i++) {
         if (n % i == 0) {
             factors.push_back(i);
         }
@@ -36,12 +30,15 @@ int main ()
 
     vector<ll> dp;
 
-    for (ll i = 0; i < factors.size(); ++i) {
-        ll curr = powers[factors[i]];
-        for (ll j = 0; j < i; ++j) {
+    for (ll i = 0; i < factors.size(); i++) {
+        ll curr = modExp(factors[i], m);
+        for (ll j = 0; j < i; j++) {
             if (factors[i] % factors[j] == 0) {
                 curr = curr - dp[j];
             }
+        }
+        while (curr < 0) {
+            curr = curr + m;
         }
         dp.push_back(curr);
     }
