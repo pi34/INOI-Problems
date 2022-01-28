14cb849c67adb60fb51e36ce563acac5ba86b379
Riya Manchanda <59563727+pi34@users.noreply.github.com>
2022-01-28 14:34:50 +0530

    Create periodicStrings.cpp

---
+++ periodicStrings.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+
+int modExp (int pow, int m) 
+{
+    int res = 1;
+    for (int i = 1; i <= pow; ++i) {
+        res = res * 2;
+        res = res % m;
+    }
+    return res;
+}
+
+int main ()
+{
+    ll n, m;
+    cin >> n >> m;
+
+    ll powers[n];
+
+    for (int i = 0; i <= n; ++i) {
+        powers[i] = modExp(i, m);
+    }
+
+    vector<ll> factors;
+
+    for (ll i = 1; i <= n; ++i) {
+        if (n % i == 0) {
+            factors.push_back(i);
+        }
+    }
+
+    vector<ll> dp;
+
+    for (ll i = 0; i < factors.size(); ++i) {
+        ll curr = powers[factors[i]];
+        for (ll j = 0; j < i; ++j) {
+            if (factors[i] % factors[j] == 0) {
+                curr = curr - dp[j];
+            }
+        }
+        dp.push_back(curr);
+    }
+
+    cout << dp[dp.size() - 1] << endl;
+
+}
